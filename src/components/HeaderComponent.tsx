f893d9db401bf7da5719b73ec542588804afac4e
Axelvazslima <axelvaz45@gmail.com>
2025-02-28 01:11:20 -0300

    menu fixing

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -4,10 +4,10 @@ import DeviceAwareComponent from "./DeviceAwareComponent";
 function Header() {
   const [menu, setMenu] = useState(false);
   return (
-    <header className={`flex ${menu ? "justify-items-stretch" : "justify-between"} items-center w-screen h-4 bg-[#0F172A] text-gray-200 p-8 font-mono`}>
+    <header className={`flex ${menu ? "justify-end" : "justify-between"} items-center w-screen h-4 bg-[#0F172A] text-gray-200 p-8 font-mono`}>
       <a href="/">
       <p className={`text-xl ${menu ? "hidden" : "block"}`}>AV</p></a>
-      <button className={`${menu ? "block" : "hidden"} bg-inherit shadow-lg shadow-blue-900 border-2 p-2 rounded-lg border-gray-100`} onClick={() => setMenu(!menu)}>Close</button>
+      <button className={`${menu ? "block" : "hidden"} bg-inherit shadow-sm shadow-blue-900 border-2 p-2 mr-2 rounded-lg border-gray-100`} onClick={() => setMenu(!menu)}>Close</button>
         <DeviceAwareComponent notebookContent={ 
         <div className="flex">
             <a href="/projects" className="md:mr-4 mr-2">Projects</a>
@@ -15,7 +15,7 @@ function Header() {
             <a href="/posts">Posts</a>
         </div>} mobileContent={
           <><button className={`${menu ? "hidden" : "block"} shadow-lg shadow-blue-900 border-2 p-2 rounded-lg border-gray-100`} onClick={() => setMenu(!menu)}>Menu</button>
-          <div className={`${menu ? "flex justify-between" : "hidden"} font-stretch-50% font-strech-ultra-condensed`}>
+          <div className={`${menu ? "flex justify-between flex-wrap font-stretch-100% text-sm" : "hidden"}`}>
             <a href="/projects" className="md:mr-4 mr-2">Projects</a>
             <a href="/certifications" className="md:mr-4 mr-2">Certifications</a>
             <a href="/posts">Posts</a>
