44c588f35aaeb2f7cef0a1192f1b1254ac784191
Axelvazslima <axelvaz45@gmail.com>
2025-02-28 00:59:05 -0300

    Fixed menu

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -7,6 +7,6 @@ interface ButtonComponentProps {
 }
 
 function ButtonComponent({ onClick, children }: ButtonComponentProps) {
-  return <button className="md:bg-[#0F172A] md:transition-all md:duration-300 md:ease-in-out md:hover:bg-purple-800 bg-inherit md:hover:shadow-xl shadow-lg shadow-blue-900 md:shadow-none text-gray-200 p-2 rounded-md border-2 border-gray-200 cursor-pointer md:hover:scale-120 text-center h-10 w-20" onClick={onClick}>{children}</button>;
+  return <button className="md:bg-[#0F172A] md:transition-all md:duration-300 md:ease-in-out md:hover:bg-purple-800 bg-inherit md:hover:shadow-xl md:shadow-none text-gray-200 p-2 rounded-md border-2 border-gray-200 cursor-pointer md:hover:scale-120 text-center h-10 w-20" onClick={onClick}>{children}</button>;
 }
 export default ButtonComponent;
--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import ButtonComponent from "./ButtonComponent";
 import DeviceAwareComponent from "./DeviceAwareComponent";
 
 function Header() {
@@ -7,13 +6,14 @@ function Header() {
   return (
     <header className="flex justify-between items-center w-screen h-4 bg-[#0F172A] text-gray-200 p-8 font-mono">
       <p className={`text-xl ${menu ? "hidden" : "block"}`}>AV</p>
+      <button className={`${menu ? "block" : "hidden"} bg-inherit shadow-lg shadow-blue-900 border-2 p-2 rounded-lg border-gray-100`} onClick={() => setMenu(!menu)}>Close</button>
         <DeviceAwareComponent notebookContent={ 
         <div className="flex">
             <a href="/projects" className="md:mr-4 mr-2">Projects</a>
             <a href="/certifications" className="md:mr-4 mr-2">Certifications</a>
             <a href="/posts">Posts</a>
         </div>} mobileContent={
-          <><ButtonComponent className={`${menu ? "hidden" : "block"}`} onClick={() => setMenu(!menu)}>{menu ? "❌" : "Menu"}</ButtonComponent>
+          <><button className={`${menu ? "hidden" : "block"} shadow-lg shadow-blue-900 border-2 p-2 rounded-lg border-gray-100`} onClick={() => setMenu(!menu)}>Menu</button>
           <div className={`${menu ? "flex justify-between" : "hidden"}`}>
             <a href="/projects" className="md:mr-4 mr-2">Projects</a>
             <a href="/certifications" className="md:mr-4 mr-2">Certifications</a>
