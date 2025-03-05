7b99159a13ffb517c3d65b952f07db082f20ac02
Axelvazslima <axelvaz45@gmail.com>
2025-03-04 23:46:55 -0300

    Data.tsx

---
+++ src/classes/Data.tsx
@@ -0,0 +1,30 @@
+export type DataType = 'post' | 'project' | 'certification';
+
+export type baseData = {
+    id: number;
+    title: string;
+    description: string;
+}
+
+export type Post = baseData & {
+    content: string;
+    date: string;
+    tags: string[];
+};
+  
+export type Project = baseData & {
+    company: string;
+    content: string;
+    image: string;
+    skills: string[];
+    tags: string[];
+    link: string;
+};
+
+export type Certification = baseData & {
+    company: string;
+    tags: string[];
+    link: string;
+};
+
+export type Data = Post | Project | Certification;
